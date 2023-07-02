e47c915f54e12849fbb2f3cbc6da857190c4b07b
Son Do <dominhson580@gmail.com>
2023-07-02 16:48:39 +0700

    Create function to display feedback

--- 2-hangman/main.py
+++ 2-hangman/main.py
@@ -1,9 +1,11 @@
 import random
 import string
+
 from hangman_pics import HANGMAN_PICS
 from secret_words import WORDS
 
 all_letters = string.ascii_lowercase
+max_num_guesses = len(HANGMAN_PICS)
 
 
 def get_random_word(words):
@@ -22,3 +24,18 @@ def take_guess(already_guessed):
             print('You already guessed that letter')
         else:
             return guess
+
+
+def get_result(secret_word, current, missed_letters, guess):
+    if guess not in secret_word:
+        missed_letters += guess
+    else:
+        for i in range(len(secret_word)):
+            if secret_word[i] == guess:
+                current[i] = guess
+    return (missed_letters, current)
+
+
+def display_feedback(missed_letters, current):
+    print(f'Missed letters: {missed_letters}')
+    print(f'Current: {current}')
