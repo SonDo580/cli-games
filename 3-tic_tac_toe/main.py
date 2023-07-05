5506470648d5d9fb317cb592ff73f0572c89ce28
Son Do <dominhson580@gmail.com>
2023-07-05 22:48:46 +0700

    Check for winner after each move

--- 3-tic_tac_toe/main.py
+++ 3-tic_tac_toe/main.py
@@ -1,6 +1,7 @@
 import random
 
 SIZE = 3
+NUM_WIN = 3
 
 
 def create_board():
@@ -32,6 +33,90 @@ def get_marks():
         return (user_mark, computer_mark)
 
 
+def check_horizontal(board, mark, row, col):
+    point = 0
+    j = col
+    while j > 0:
+        j -= 1
+        if board[row][j] == mark:
+            point += 1
+
+    j = col
+    while j < SIZE - 1:
+        j += 1
+        if board[row][j] == mark:
+            point += 1
+
+    return point + 1
+
+
+def check_vertical(board, mark, row, col):
+    point = 0
+    i = row
+    while i > 0:
+        i -= 1
+        if board[i][col] == mark:
+            point += 1
+
+    i = row
+    while i < SIZE - 1:
+        i += 1
+        if board[i][col] == mark:
+            point += 1
+
+    return point + 1
+
+
+def check_northwest_southeast(board, mark, row, col):
+    point = 0
+    i = row
+    j = col
+    while i > 0 and j > 0:
+        i -= 1
+        j -= 1
+        if board[i][j] == mark:
+            point += 1
+
+    i = row
+    j = col
+    while i < SIZE - 1 and j < SIZE - 1:
+        i += 1
+        j += 1
+        if board[i][j] == mark:
+            point += 1
+
+    return point + 1
+
+
+def check_northeast_southwest(board, mark, row, col):
+    point = 0
+    i = row
+    j = col
+    while i > 0 and j < SIZE - 1:
+        i -= 1
+        j += 1
+        if board[i][j] == mark:
+            point += 1
+
+    i = row
+    j = col
+    while i < SIZE - 1 and j > 0:
+        i += 1
+        j -= 1
+        if board[i][j] == mark:
+            point += 1
+
+    return point + 1
+
+
+def check_board(board, mark, row, col):
+    point1 = check_horizontal(board, mark, row, col)
+    point2 = check_vertical(board, mark, row, col)
+    point3 = check_northwest_southeast(board, mark, row, col)
+    point4 = check_northeast_southwest(board, mark, row, col)
+    return point1 >= NUM_WIN or point2 >= NUM_WIN or point3 >= NUM_WIN or point4 >= NUM_WIN
+
+
 def get_random_int(min, max):
     return random.randint(min, max)
 
@@ -40,10 +125,9 @@ def computer_move(board, computer_mark):
     while True:
         row = get_random_int(0, SIZE - 1)
         col = get_random_int(0, SIZE - 1)
-
         if board[row][col] == ' ':
             board[row][col] = computer_mark
-            return
+            return check_board(board, computer_mark, row, col)
 
 
 def user_move(board, user_mark):
@@ -52,13 +136,11 @@ def user_move(board, user_mark):
         try:
             row = int(input("Enter row index: "))
             col = int(input("Enter column index: "))
-
             if board[row][col] != ' ':
                 print('Please choose another cell!')
                 continue
-
             board[row][col] = user_mark
-            return
+            return check_board(board, user_mark, row, col)
         except ValueError:
             print('Please enter an interger!')
 
